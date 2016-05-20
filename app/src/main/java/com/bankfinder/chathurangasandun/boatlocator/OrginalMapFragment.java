b0bdfb2d1db875dcbe2f088559d3884d9df588f5
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2016-05-20 09:39:22 +0530

    map is ok

--- app/src/main/java/com/bankfinder/chathurangasandun/boatlocator/OrginalMapFragment.java
+++ app/src/main/java/com/bankfinder/chathurangasandun/boatlocator/OrginalMapFragment.java
@@ -1,12 +1,30 @@
 package com.bankfinder.chathurangasandun.boatlocator;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.offline.OfflineManager;
+import com.mapbox.mapboxsdk.offline.OfflineRegion;
+import com.mapbox.mapboxsdk.offline.OfflineRegionError;
+import com.mapbox.mapboxsdk.offline.OfflineRegionStatus;
+import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
+
+import org.json.JSONObject;
 
 
 /**
@@ -17,7 +35,7 @@ import android.view.ViewGroup;
  * Use the {@link OrginalMapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OrginalMapFragment extends Fragment {
+public class OrginalMapFragment extends Fragment implements OnMapReadyCallback {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,6 +46,26 @@ public class OrginalMapFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+
+    private View v;
+
+
+
+    private static final String TAG = "MainActivity";
+
+    private boolean isEndNotified;
+    private ProgressBar progressBar;
+    private MapView mapView;
+
+    // JSON encoding/decoding
+    public final static String JSON_CHARSET = "UTF-8";
+    public final static String JSON_FIELD_REGION_NAME = "FIELD_REGION_NAME";
+    private MapboxMap mapboxMap;
+
+
+    SharedPreferences downloadvalue;
+    SharedPreferences.Editor edit;
 
     public OrginalMapFragment() {
         // Required empty public constructor
@@ -58,13 +96,50 @@ public class OrginalMapFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orginal_map, container, false);
+        v = inflater.inflate(R.layout.fragment_orginal_map, container, false);
+
+
+        downloadvalue = getContext().getSharedPreferences("DOWNLOADVALUE", getContext().MODE_PRIVATE);
+
+        edit = downloadvalue.edit();
+        edit.commit();
+
+        Log.d(TAG+"value-1", downloadvalue.getInt("VALUE",0)+"");
+
+
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+
+
+        if(downloadvalue.getInt("VALUE",0) != 100){
+            downloadRegion();
+        }else{
+            Log.d(TAG, "completed");
+        }
+
+
+
+
+        return v;
+
+
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,6 +166,7 @@ public class OrginalMapFragment extends Fragment {
         mListener = null;
     }
 
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -105,4 +181,149 @@ public class OrginalMapFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    // Progress bar methods
+    private void startProgress() {
+
+        // Start and show the progress bar
+        isEndNotified = false;
+        progressBar.setIndeterminate(true);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void setPercentage(final int percentage) {
+        progressBar.setIndeterminate(false);
+        progressBar.setProgress(percentage);
+    }
+
+    private void endProgress(final String message) {
+        // Don't notify more than once
+        if (isEndNotified) return;
+
+        // Stop and hide the progress bar
+        isEndNotified = true;
+        progressBar.setIndeterminate(false);
+        progressBar.setVisibility(View.GONE);
+
+        // Show a toast
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+
+
+    @Override
+    public void onMapReady(MapboxMap mb) {
+        mapboxMap = mb;
+
+
+
+
+
+
+
+    }
+
+    private void downloadRegion(){
+
+        // Set up the OfflineManager
+        OfflineManager offlineManager = OfflineManager.getInstance(getActivity());
+        offlineManager.setAccessToken(getString(R.string.accessToken));
+
+        // Create a bounding box for the offline region
+        LatLngBounds latLngBounds = new LatLngBounds.Builder()
+                .include(new LatLng(11.603215, 76.534143)) // Northeast
+                .include(new LatLng(7.394992, 80.937680)) // Southwest
+                .build();
+
+        // Define the offline region
+        OfflineTilePyramidRegionDefinition definition = new OfflineTilePyramidRegionDefinition(
+                mapView.getStyleUrl(),
+                latLngBounds,
+                0,
+                12,
+                this.getResources().getDisplayMetrics().density);
+
+        // Set the metadata
+        byte[] metadata;
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put(JSON_FIELD_REGION_NAME, "1");
+            String json = jsonObject.toString();
+            metadata = json.getBytes(JSON_CHARSET);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to encode metadata: " + e.getMessage());
+            metadata = null;
+        }
+
+        // Create the region asynchronously
+        offlineManager.createOfflineRegion(definition, metadata, new OfflineManager.CreateOfflineRegionCallback() {
+            @Override
+            public void onCreate(OfflineRegion offlineRegion) {
+                offlineRegion.setDownloadState(OfflineRegion.STATE_ACTIVE);
+
+                // Display the download progress bar
+                progressBar = (ProgressBar) v.findViewById(R.id.progress_bar);
+                startProgress();
+
+                // Monitor the download progress using setObserver
+                offlineRegion.setObserver(new OfflineRegion.OfflineRegionObserver() {
+                    @Override
+                    public void onStatusChanged(OfflineRegionStatus status) {
+
+                        // Calculate the download percentage and update the progress bar
+                        double percentage = status.getRequiredResourceCount() >= 0 ?
+                                (100.0 * status.getCompletedResourceCount() / status.getRequiredResourceCount()) :
+                                0.0;
+
+                        if (status.isComplete()) {
+                            // Download complete
+                            endProgress("Region downloaded successfully.");
+                            edit.putInt("VALUE",100);
+                            edit.commit();
+                        } else if (status.isRequiredResourceCountPrecise()) {
+                            // Switch to determinate state
+                            setPercentage((int) Math.round(percentage));
+                            edit.putInt("VALUE",(int) Math.round(percentage));
+                            edit.commit();
+                            Log.d(TAG+"value", downloadvalue.getInt("VALUE",0)+"");
+
+                        }
+                    }
+
+                    @Override
+                    public void onError(OfflineRegionError error) {
+                        // If an error occurs, print to logcat
+                        Log.e(TAG, "onError reason: " + error.getReason());
+                        Log.e(TAG, "onError message: " + error.getMessage());
+                    }
+
+                    @Override
+                    public void mapboxTileCountLimitExceeded(long limit) {
+                        // Notify if offline region exceeds maximum tile count
+                        Log.e(TAG, "Mapbox tile count limit exceeded: " + limit);
+                    }
+                });
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.e(TAG, "Error: " + error);
+            }
+        });
+    }
+
+
 }
