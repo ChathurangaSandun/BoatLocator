2e789778c845b1f46df9b4b495570391a71e3421
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2016-06-03 15:24:57 +0530

    set round button and name

--- app/src/main/java/com/bankfinder/chathurangasandun/boatlocator/OwnerActivity.java
+++ app/src/main/java/com/bankfinder/chathurangasandun/boatlocator/OwnerActivity.java
@@ -1,13 +1,28 @@
 package com.bankfinder.chathurangasandun.boatlocator;
 
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class OwnerActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,83 @@ public class OwnerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.background);
+        Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 180);
+
+        ImageView circularImageView = (ImageView)findViewById(R.id.circleView);
+        circularImageView.setImageBitmap(circularBitmap);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(new String[]{"a","b","c","d"});
+        mRecyclerView.setAdapter(mAdapter);
+
+
+
+
+
+
+
     }
 
+    private static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.my_text_view, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+
+            ViewHolder vh = new ViewHolder((TextView) v.findViewById(R.id.textView));
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
 }
---
+++ app/src/main/java/com/bankfinder/chathurangasandun/boatlocator/OwnerAdapter.java
@@ -0,0 +1,7 @@
+package com.bankfinder.chathurangasandun.boatlocator;
+
+/**
+ * Created by Chathuranga Sandun on 6/3/2016.
+ */
+public class OwnerAdapter {
+}
