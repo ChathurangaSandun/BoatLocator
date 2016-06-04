cadaf1e8dfb665bc911a399836345729f95cc86d
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2016-06-04 19:06:08 +0530

    get device id and gmail account

---
+++ app/src/main/java/com/bankfinder/chathurangasandun/boatlocator/parse/ParseConstrains.java
@@ -0,0 +1,13 @@
+package com.bankfinder.chathurangasandun.boatlocator.parse;
+
+/**
+ * Created by Chathuranga Sandun on 6/4/2016.
+ */
+public class ParseConstrains {
+
+    public static String APPLICATION_KEY = "";
+    public static String CLIENT_KEY = "";
+
+
+
+}
